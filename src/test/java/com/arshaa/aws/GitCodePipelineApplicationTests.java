22be7d2762695041220d7646fa343aa7f6a0399c
NobithaBhavani <bhavani.gudala421@gmail.com>
2021-11-12 16:07:47 +0530

    first commit

---
+++ src/test/java/com/arshaa/aws/GitCodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arshaa.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
